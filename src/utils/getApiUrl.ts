65a0cc49c7c2692e2669f0158b4da3337c8f66f5
nikko@gappify.com <kertnik@gmail.com>
2024-05-13 19:00:53 +0800

    add login page

--- src/utils/getApiUrl.ts
+++ src/utils/getApiUrl.ts
@@ -3,7 +3,7 @@ import { Local } from '@/constants/api-urls/Local'
 
 export function getApiUrl(hostname: string) {
 	if (hostname === CLIENT_URL.DEV) {
-		return null
+		return Local // TODO: Add DEV URL
 	}
 
 	return Local
