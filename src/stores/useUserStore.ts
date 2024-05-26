23024cfabb040fd1291470c1d005ef9d9cbacd6b
Nikko Lumahang <kertnik@gmail.com>
2024-05-26 16:36:48 +0800

    update users list fetching

--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -4,17 +4,21 @@ import { create } from 'zustand'
 interface UserStoreProps {
 	users: User[]
 	page: number
+	perPage: number
 	totalPages: number
 	setUsers: (users: User[]) => void
 	setPage: (page: number) => void
+	setPerPage: (perPage: number) => void
 	setTotalPages: (totalPages: number) => void
 }
 
 export const useUserStore = create<UserStoreProps>(set => ({
 	users: [],
 	page: 1,
+	perPage: 20,
 	totalPages: 1,
 	setUsers: (users: User[]) => set({ users }),
 	setPage: (page: number) => set({ page }),
+	setPerPage: (perPage: number) => set({ perPage }),
 	setTotalPages: (totalPages: number) => set({ totalPages }),
 }))
